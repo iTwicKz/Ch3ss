52d8b6fe203a491eef223e8c120e6df8966cc1c2
cardar777 <darianjmiranda@ufl.edu>
2015-04-11 17:15:04 -0400

    Update main.cpp to handle clicks
    clicks are handled in Board.cpp which handles what the hell to do with the input in terms of the Player and Piece algorithms

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,19 @@ using namespace cimg_library;
 
 int main() 
 {	
-	int x = 0;//x coordinate on grid to convert to the array divide by 60
-	int y = 0;//y coordinate on grid to convert to the array divide by 60
-	setup();
+	setup();//setup board
 	while (!main_disp.is_closed()) //display loop until closed
 	{
 		main_disp.wait();
 		if (main_disp.button() && main_disp.mouse_y()>=0) //if mouse is clicked on screen
 		{
-			x = main_disp.mouse_x();//get x coordinate
-			y = main_disp.mouse_y();//get y coordinate
-			movedraw(bpawn, 0, 0, x/60, y/60, false);
-			updateboard();
+		const int x = main_disp.mouse_x();//get x coordinate
+		const int y = main_disp.mouse_y();//get y coordinate
+		handleclick(x/60,y/60);
+		updateboard();
 		}
 	}
 	return 0;
 }
+
+
