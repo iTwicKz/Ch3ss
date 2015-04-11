7bae6c88ceee7ecf9706e40aad188fe532fc2e2b
cardar777 <darianjmiranda@ufl.edu>
2015-04-11 16:42:58 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,18 @@ using namespace cimg_library;
 
 int main() 
 {	
+	int x = 0;//x coordinate on grid to convert to the array divide by 60
+	int y = 0;//y coordinate on grid to convert to the array divide by 60
 	setup();
 	while (!main_disp.is_closed()) //display loop until closed
 	{
 		main_disp.wait();
 		if (main_disp.button() && main_disp.mouse_y()>=0) //if mouse is clicked on screen
 		{
-		const int x = main_disp.mouse_x();//get x coordinate
-		const int y = main_disp.mouse_y();//get y coordinate
-		
-		updateboard();
+			x = main_disp.mouse_x();//get x coordinate
+			y = main_disp.mouse_y();//get y coordinate
+			movedraw(bpawn, 0, 0, x/60, y/60, false);
+			updateboard();
 		}
 	}
 	return 0;
