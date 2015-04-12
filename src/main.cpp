ea9a884757a6f2a6a4bc75406e3b4c64f83a6871
cardar777 <darianjmiranda@ufl.edu>
2015-04-11 20:21:00 -0400

    Update main.cpp
    TEST CODE

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,9 @@ using namespace cimg_library;
 
 int main() 
 {	
+	bool selectedalready = false;
 	setup();//setup board
+	updateboard();
 	while (!main_disp.is_closed()) //display loop until closed
 	{
 		main_disp.wait();
@@ -12,11 +14,18 @@ int main()
 		{
 		const int x = main_disp.mouse_x();//get x coordinate
 		const int y = main_disp.mouse_y();//get y coordinate
-		handleclick(x/60,y/60);
-		updateboard();
+		
+		//TODO NEEDS TO WORK WITH PIECE OBJECTS
+		//TEST CODE ONLY!
+		
+		handleclick(returnpiece(x/60,y/60),x/60,y/60,selectedalready);
+		selectedalready = !selectedalready;//flips
+		
+		//END TEST CODE
+		
+		
+		
 		}
 	}
 	return 0;
 }
-
-
