fbb3262694a56d782627d2817cfe9197d1846df0
besant <besantnandra@gmail.com>
2015-04-11 22:27:02 -0400

    Create collisionTest.cpp
    integration

---
+++ src/collisionTest.cpp
@@ -0,0 +1,64 @@
+#include <cmath> 
+#include <string>
+#include <iostream>
+using namespace std;
+int board[8][8];
+//int move[2] = {4,2};
+//int position[2] = {6,3};
+bool collision (int move[], int position[]) { //Method that returns free as true or false
+	int moveSpacesHor = move[0] - position[0];
+	int moveSpacesVer = move[1] - position[1];
+	bool free = true;
+	if (abs(moveSpacesHor) == abs(moveSpacesVer)) { //Checks if diagnol
+		if (moveSpacesHor == moveSpacesVer && moveSpacesHor > 0) {	//diagnol + +
+			for ( int i = 1; i<abs(moveSpacesHor); i++) {
+				if (board[position[0] + i][position[1] + i] != 153) {
+					free = false;
+				}
+			}
+		} else if (moveSpacesHor == moveSpacesVer && moveSpacesHor < 0) { //diagnol - -
+			for ( int i = 1; i<abs(moveSpacesHor); i++) {
+				if (board[position[0] - i][position[1] - i] != 153) {
+					free = false;
+				}
+			}
+		} else if (abs(moveSpacesHor) == moveSpacesVer) { //diagnol - +
+			for ( int i = 1; i<abs(moveSpacesHor); i++) {
+				if (board[position[0] - i][position[1] + i] != 153) {
+					free = false;
+				}
+			}
+		} else if (moveSpacesHor == abs(moveSpacesVer)) { //diagnol + -
+			for ( int i = 1; i<abs(moveSpacesHor); i++) {
+				if (board[position[0] + i][position[1] - i] != 153) {
+					free = false;
+				}
+			}
+		}
+	} else if ((moveSpacesHor = 0) && (moveSpacesVer > 0)) { //Vertical +
+		for ( int i = 1; i<moveSpacesVer; i++) {
+			if (board[position[0]][position[1] + i] != 153) {
+					free = false;
+				}
+			}
+	} else if ((moveSpacesHor = 0) && (moveSpacesVer < 0)) { //Vertical -
+		for ( int i = 1; i<abs(moveSpacesVer); i++) {
+			if (board[position[0]][position[1] - i] != 153) {
+					free = false;
+				}
+			}
+	} else if ((moveSpacesVer = 0) && (moveSpacesHor > 0)) { //Horizontal +
+		for ( int i = 1; i<moveSpacesHor; i++) {
+			if (board[position[0] + 1][position[1]] != 153) {
+					free = false;
+				}
+			}
+	} else if ((moveSpacesVer = 0) && (moveSpacesHor < 0)) { //Horizontal -
+		for ( int i = 1; i<abs(moveSpacesHor); i++) {
+			if (board[position[0] - 1][position[1]] != 153) {
+				free = false;
+				}
+			}
+	}
+	return free;
+}
