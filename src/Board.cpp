0c5f63d14aaa6677075ec9dd6c160f52d948d8a5
cardar777 <darianjmiranda@ufl.edu>
2015-04-13 16:06:36 -0400

    Update Board.cpp
    Added castling and readability support

--- src/Board.cpp
+++ src/Board.cpp
@@ -26,6 +26,7 @@
 #define ROOK 3
 #define QUEEN 4
 #define KING 5
+#define PIXELSQUARESIZE 60
 
 using namespace cimg_library;
 
@@ -56,8 +57,8 @@ int boardarray[8][8];
 Piece piecearray[8][8];
 
 //last coordinates start outside board
-int lastx = 8;
-int lasty = 8;
+int lastx = 9;
+int lasty = 9;
 int lastp = 0;
 
 //to write to screen/debug
@@ -73,7 +74,8 @@ void updateboard()
 
 int returnpiece(int x, int y)//returns piece at spot on board given
 {
-	return piecearray[x][y].getType();
+	return(boardarray[x][y]);
+	//return piecearray[x][y].getType();
 }
 
 void setup()
@@ -84,99 +86,123 @@ void setup()
 		for(int j = 0; j < 8; j++)
 		{
 			boardarray[i][j] = 0;
-			//piecearray[i][j].changeType(BLANK);//TODO FIX
+			piecearray[i][j].changeType(BLANK);//TODO FIX
 		}
 	//white
-	chessboard.draw_image(0,420,wrook);
+	chessboard.draw_image(0,7*PIXELSQUARESIZE,wrook);
 	boardarray[0][7] = WHITEROOK;
-	chessboard.draw_image(60,420,wknight);
+	chessboard.draw_image(1*PIXELSQUARESIZE,7*PIXELSQUARESIZE,wknight);
 	boardarray[1][7] = WHITEKNIGHT;
-	chessboard.draw_image(120,420,wbishop);
+	chessboard.draw_image(2*PIXELSQUARESIZE,7*PIXELSQUARESIZE,wbishop);
 	boardarray[2][7] = WHITEBISHOP;
-	chessboard.draw_image(180,420,wqueen);
+	chessboard.draw_image(3*PIXELSQUARESIZE,7*PIXELSQUARESIZE,wqueen);
 	boardarray[3][7] = WHITEQUEEN;
-	chessboard.draw_image(240,420,wking);
+	chessboard.draw_image(4*PIXELSQUARESIZE,7*PIXELSQUARESIZE,wking);
 	boardarray[4][7] = WHITEKING;
-	chessboard.draw_image(300,420,wbishop);
+	chessboard.draw_image(5*PIXELSQUARESIZE,7*PIXELSQUARESIZE,wbishop);
 	boardarray[5][7] = WHITEBISHOP;
-	chessboard.draw_image(360,420,wknight);
+	chessboard.draw_image(6*PIXELSQUARESIZE,7*PIXELSQUARESIZE,wknight);
 	boardarray[6][7] = WHITEKNIGHT;
-	chessboard.draw_image(420,420,wrook);
+	chessboard.draw_image(7*PIXELSQUARESIZE,7*PIXELSQUARESIZE,wrook);
 	boardarray[7][7] = WHITEROOK;
 	for(int i = 0; i <=7; i++)
 	{
-		chessboard.draw_image(i*60,360,wpawn);
+		chessboard.draw_image(i*PIXELSQUARESIZE,6*PIXELSQUARESIZE,wpawn);
 		boardarray[i][6] = WHITEPAWN;
 	}
 		
 	//black
 	chessboard.draw_image(0,0,brook);
 	boardarray[0][0] = BLACKROOK;
-	chessboard.draw_image(60,0,bknight);
+	chessboard.draw_image(1*PIXELSQUARESIZE,0,bknight);
 	boardarray[1][0] = BLACKKNIGHT;
-	chessboard.draw_image(120,0,bbishop);
+	chessboard.draw_image(2*PIXELSQUARESIZE,0,bbishop);
 	boardarray[2][0] = BLACKBISHOP;
-	chessboard.draw_image(180,0,bqueen);
+	chessboard.draw_image(3*PIXELSQUARESIZE,0,bqueen);
 	boardarray[3][0] = BLACKQUEEN;
-	chessboard.draw_image(240,0,bking);
+	chessboard.draw_image(4*PIXELSQUARESIZE,0,bking);
 	boardarray[4][0] = BLACKKING;
-	chessboard.draw_image(300,0,bbishop);
+	chessboard.draw_image(5*PIXELSQUARESIZE,0,bbishop);
 	boardarray[5][0] = BLACKBISHOP;
-	chessboard.draw_image(360,0,bknight);
+	chessboard.draw_image(6*PIXELSQUARESIZE,0,bknight);
 	boardarray[6][0] = BLACKKNIGHT;
-	chessboard.draw_image(420,0,brook);
+	chessboard.draw_image(7*PIXELSQUARESIZE,0,brook);
 	boardarray[7][0] = BLACKROOK;
 	for(int i = 0; i <=7; i++)
 	{
-		chessboard.draw_image(i*60,60,bpawn);
+		chessboard.draw_image(i*PIXELSQUARESIZE,60,bpawn);
 		boardarray[i][1] = BLACKPAWN;
 	}
 	updateboard();
 }
 
-void movedraw(CImg<unsigned char> piece, int srcx, int srcy, int destx, int desty, bool special)//x and y are 0-7
+void movedraw(CImg<unsigned char> piece, int srcx, int srcy, int destx, int desty)//x and y are 0-7
 {	
-	if(!special)//a special move is the en passant or castling or queening
+	//check for the special moves en passant or castling or queening or double pawn open
+	//Castling move was checked as valid in handleclick
+	if((piece == bking && srcx+2 == destx) || (piece == wking && srcx+2 == destx))//only time the king can move 2
 	{
-		chessboard.draw_image(destx*60, desty*60, piece);
-		//determine if square is light or dark
-		if(srcx%2 == 0 && srcy%2 == 0)
-			chessboard.draw_image(srcx*60,srcy*60,lsquare);
-		else if(srcx%2 == 0 && srcy != 0)
-			chessboard.draw_image(srcx*60,srcy*60,dsquare);
-		else if(srcx%2 != 0 && srcy%2 == 0)
-			chessboard.draw_image(srcx*60,srcy*60,dsquare);
-		else if(srcx%2 != 0 && srcy%2 != 0)
-			chessboard.draw_image(srcx*60,srcy*60,lsquare);
-		
-		
-		updateboard();	
-		boardarray[destx][desty] = boardarray[srcx][srcy];
-		boardarray[srcx][srcx] = BLANK;
-	}
-	else//Assume that if command for special move is passed the requirements are met
-	{
-		//Castling
-		if(piece == bking || piece == wking)
+		chessboard.draw_image(destx*PIXELSQUARESIZE, desty*PIXELSQUARESIZE, piece);
+		if(piece == wking)//done this way to prevent falsely identified castling
 		{
-			//move rook to spot on side of king
-			//left
-			if(srcx < destx)
-			{
-				//
-			}
-			//right
-			else if(srcx > destx)
-			{
-				//
-			}
+			chessboard.draw_image(300, desty*PIXELSQUARESIZE, wrook);
+			chessboard.draw_image(420, desty*PIXELSQUARESIZE, lsquare);
+		}
+		if(piece == bking)
+		{
+			chessboard.draw_image(300, desty*PIXELSQUARESIZE, brook);
+			chessboard.draw_image(420, desty*PIXELSQUARESIZE, dsquare);
 		}
 	}
+	else if((piece == bking && srcx == destx+2) || (piece == wking && srcx == destx+2))
+	{
+		chessboard.draw_image(destx*PIXELSQUARESIZE, desty*PIXELSQUARESIZE, piece);
+		if(piece == wking)//done this way to prevent falsely identified castling
+		{
+			chessboard.draw_image(240, desty*PIXELSQUARESIZE, wrook);
+			chessboard.draw_image(0, desty*PIXELSQUARESIZE, dsquare);
+		}
+		if(piece == bking)
+		{
+			chessboard.draw_image(240, desty*PIXELSQUARESIZE, brook);
+			chessboard.draw_image(0, desty*PIXELSQUARESIZE, lsquare);
+		}
+	}
+	//The double pawn opening
+	else if((piece == bpawn || piece == wpawn) && (srcy == || srcy ==) && (desty == ||))
+	{
+		//
+	}
+	//Queening a pawn
+	else if(piece == bpawn && desty == 7)
+	{
+		//
+	}
+	else if(piece == wpawn && desty == 0)
+	{
+		//
+	}
+	else
+		chessboard.draw_image(destx*PIXELSQUARESIZE, desty*PIXELSQUARESIZE, piece);
+	//determine if square is light or dark
+		if(srcx%2 == 0 && srcy%2 == 0)
+			chessboard.draw_image(srcx*PIXELSQUARESIZE,srcy*PIXELSQUARESIZE,lsquare);
+		else if(srcx%2 == 0 && srcy != 0)
+			chessboard.draw_image(srcx*PIXELSQUARESIZE,srcy*PIXELSQUARESIZE,dsquare);
+		else if(srcx%2 != 0 && srcy%2 == 0)
+			chessboard.draw_image(srcx*PIXELSQUARESIZE,srcy*PIXELSQUARESIZE,dsquare);
+		else if(srcx%2 != 0 && srcy%2 != 0)
+			chessboard.draw_image(srcx*PIXELSQUARESIZE,srcy*PIXELSQUARESIZE,lsquare);
+		
+		
+		updateboard();
+		boardarray[destx][desty] = boardarray[srcx][srcy];//move piece to new position
+		boardarray[srcx][srcx] = BLANK;//override old one
 }
 
-void handleclick(int p, int x, int y, bool b)//kinda working
+void handleclick(int p, int x, int y, bool select)//kinda working
 {
-	if(b && (lastp != BLANK))
+	if(select && (lastp != BLANK))//if they select a piece of their color
 	{
 		//check if valid move
 		CImg<unsigned char> piece;
@@ -204,7 +230,9 @@ void handleclick(int p, int x, int y, bool b)//kinda working
 			piece = brook;
 		if(lastp == BLACKPAWN)
 			piece = bpawn;
-		movedraw(piece,lastx,lasty,x,y,false);
+		//CHECK VALID MOVE FIRST
+			movedraw(piece,lastx,lasty,x,y);
+		//
 	}
 	else
 	{
