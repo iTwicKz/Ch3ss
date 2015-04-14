dfef2f16f1efa5dc98647f588351a474f0bc72ae
besant <besantnandra@gmail.com>
2015-04-13 23:57:59 -0400

    Update Rook.cpp

--- src/Old Separate Files/Rook.cpp
+++ src/Old Separate Files/Rook.cpp
@@ -1,43 +1,40 @@
-#include <string>
-#include <vector>
-#include <iostream>
-#include <cmath>
-#include "Piece.h"
-#include "Piece.cpp"
-using namespace std;
+bool Rook::movePiece() {
 	
-void Rook::movePiece() {
+	bool valid = false;
+	
+	bool firstMoved = true;
 
 	if(move[0] != position[0] && move[1] != position[1]){	//checks if user has set move to current space
 		int moveSpacesHor = move[0] - position[0];
 		int moveSpacesVer = move[1] - position[1];
 		
-		if(!collisionPath()){				//if there is NOT an object in its path
-			if( moveSpacesHor!=0 && move[1] == position[1]) {
-				if(collisionAttack()){
-					kill(move);
+		if(collision(move, position)){				//if there is NOT an object in its path
+			if(moveSpacesHor!=0 && move[1] == position[1]) {
+				if(collisionAttack(move)){
+					//kill(move);
 				}
 				position[0] = move[0];
 				position[1] = move[1];
 				firstMoved = false;
+				valid = true;
 				
-			} else if( moveSpacesVer !=0 && move[0] == position[0]) {
-				if( collisionAttack() ) {
-					kill(move);
+			} else if(moveSpacesVer !=0 && move[0] == position[0]) {
+				if(collisionAttack(move)) {
+					//kill(move);
 				}
 				position[0] = move[0];
 				position[1] = move[1];
 				firstMoved = false;
+				valid = true;
 				
 			} else cout<<"Brahhhhhhhhhhh you can't move dat";
+			
 			//Work on castling	
 		}
 	}
 
 	else cout<<"That is the spot you are currently in";		//Most likely simply ignore
-
-}
-
-int Rook::getType(){
-	return type;
+	
+	return valid;
+	
 }
