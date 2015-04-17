b3bc7a29d09e45bca939c0e4d4a2199f4f92ca80
cardar777 <darianjmiranda@ufl.edu>
2015-04-16 20:47:17 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include "Board.cpp"
 #include "CImg.h"
-#include "pieceTest.h"
 
+//g++ main.cpp -lpthread -lX11 
 using namespace cimg_library;
 
 int main() 
 {	
-	bool selectedalready = false;
-
+	bool player = true;//true is white false is black
+	bool selected = false;
 	setup();//setup board
-	updateboard();
+	updateBoard();
 	while (!main_disp.is_closed()) //display loop until closed
 	{
 		main_disp.wait();
@@ -18,14 +18,13 @@ int main()
 		const int x = main_disp.mouse_x();//get x coordinate
 		const int y = main_disp.mouse_y();//get y coordinate
 		
-		//TODO NEEDS TO WORK WITH PIECE OBJECTS
-		//TEST CODE ONLY!
+		selected = handleClick(returnPiece(x/PIXELSQUARESIZE,y/PIXELSQUARESIZE),x/PIXELSQUARESIZE,y/PIXELSQUARESIZE, selected);
+		if(!selected)//change player
+			player = !player;
 		
-		handleclick(returnpiece(x/60,y/60),x/60,y/60,selectedalready);
-		selectedalready = !selectedalready;//flips
-		
-		//END TEST CODE
 		}
+		updateBoard();
 	}
 	return 0;
 }
+
